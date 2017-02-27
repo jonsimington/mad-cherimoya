0da811b9fc59302c9c29f09b4c195ebd0324a7ab
Jordan Giacone <Jmgv27@mst.edu>
2017-02-26 21:36:20 -0600

    Added a function to convert from sig-game pieces

--- games/chess/ChessPiece.py
+++ games/chess/ChessPiece.py
@@ -1,3 +1,6 @@
+from games.chess.PieceType import PieceType
+
+
 class ChessPiece:
     def __init__(self):
         self.type = None
@@ -6,8 +9,18 @@ class ChessPiece:
         self.has_moved = False
         self.captured = False
         self.color = None
+        self.game_piece = None
 
     def __str__(self):
         if self.color == "White":
             return str(self.type)
         return str(self.type).lower()
+    
+    def convert_from_game_piece(self, piece):
+        self.type = PieceType[piece.type.upper()]
+        self.board_location = 8 - int(piece.rank), ord(piece.file) - ord("a")
+        self.rank_file = piece.rank, piece.file
+        self.has_moved = piece.has_moved
+        self.captured = piece.captured
+        self.color = piece.owner.color
+        self.game_piece = piece
