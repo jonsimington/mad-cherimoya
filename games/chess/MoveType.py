1e243dbc8a8f1dd805e35b11d27d71c3fdadcb2d
Jordan Giacone <Jmgv27@mst.edu>
2017-02-26 21:35:43 -0600

    Added movement tuples to the different movement types

--- games/chess/MoveType.py
+++ games/chess/MoveType.py
@@ -3,7 +3,10 @@ from enum import Enum, unique
 
 @unique
 class MoveType(Enum):
-    VERTICAL = 1
-    HORIZONTAL = 2
-    L_SHAPED = 3
-    DIAGONAL = 4
+    VERTICAL = ([(-1, 0), (1, 0)])
+    HORIZONTAL = ([(0, -1), (0, 1)])
+    L_SHAPED = ([(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)])
+    DIAGONAL = ([(-1, -1), (-1, 1), (1, -1), (1, 1)])
+
+    def __init__(self, movement_tuples):
+        self.movement_tuples = movement_tuples
