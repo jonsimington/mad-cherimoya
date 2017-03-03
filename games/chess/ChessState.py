0a892607259503935bed73ffb4f2a041f70cecc3
Jordan Giacone <Jmgv27@mst.edu>
2017-03-03 01:56:46 -0600

    Preliminary state class

---
+++ games/chess/ChessState.py
@@ -0,0 +1,9 @@
+class ChessState:
+    def __init__(self):
+        self.board = None
+        self.pieces = None
+        self.enemy_pieces = None
+        self.en_passant_enemy = None
+        self.king_board_location = None
+
+
