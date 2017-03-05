d816de6e73b00ad4171346ce5d7bdee142bd9374
Jordan Giacone <Jmgv27@mst.edu>
2017-03-04 19:03:26 -0600

    Wrote a __str__() to make moves look nicer

--- games/chess/ChessMove.py
+++ games/chess/ChessMove.py
@@ -7,3 +7,13 @@ class ChessMove:
         self.promote_to = ""
         self.en_passant = False
         self.castling = False
+
+    def __str__(self):
+        str = "{} {} -> {}".format(self.piece_moved_id, self.board_location_from, self.board_location_to)
+
+        if self.en_passant:
+            str += " (En Passant Capture)"
+        elif self.castling:
+            str += " (Castling)"
+
+        return str
